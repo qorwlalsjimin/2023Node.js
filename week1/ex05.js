5e683c072a56c7c8ccb7f9372a11bd742624dba8
Jimin Baek <qorwlalsjimin@naver.com>
2023-03-15 08:46:15 +0900

    JSON - 문자열

--- week1/ex05.js
+++ week1/ex05.js
@@ -5,3 +5,4 @@ console.log(obj.count);
 console.log(obj.result);
 
 console.log(JSON.stringify({x:5, y:6})); //값 또는 객체를 JSON 형태의 문자열로
+
---
+++ week2/ex01.js
@@ -0,0 +1,9 @@
+//JSON문자열 형식을 객체로 바꾸기
+const jsonData = '{"result":true, "number":42}';
+const obj = JSON.parse(jsonData);
+
+console.log(obj.number);
+console.log(obj.result);
+
+//값 혹은 객체를 JSON형태의 문자열로 변경
+console.log(JSON.stringify({x:4, y:2, result: true}));
