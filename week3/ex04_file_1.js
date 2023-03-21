9c7a59dc90b1ef19cc90430f767b9800263bf0d8
Jimin Baek <qorwlalsjimin@naver.com>
2023-03-22 08:56:08 +0900

    텍스트 파일에 쓰기

--- week3/ex04_file_1.js
+++ week3/ex04_file_1.js
@@ -1,9 +1,17 @@
 const fs = require('fs');
-let readDate = "읽어오지 못했습니다.";
+let readData = "읽어오지 못했습니다.";
 fs.readFile("./readText.txt", (err, data) => {
     if(err){
         throw err;
     }
     console.log(data);
     console.log(data.toString());
+    readData = data.toString();
 });
+
+fs.writeFile("./writeText.txt", ("읽어온 데이터: "+readData), (err)=>{
+    if(err){
+        throw err;
+    }
+    console.log("파일쓰기 완료");
+})
