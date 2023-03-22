3c9b9b36fde5870c90149d765b83610ca765749f
Jimin Baek <qorwlalsjimin@naver.com>
2023-03-22 09:57:17 +0900

    한 단락으로 서버 만들기

--- week3/server1.js
+++ week3/server1.js
@@ -5,14 +5,7 @@ const http = require('http');
 const server = http.createServer((req, res)=>{
     res.write("<h1> Hello Node~~</h1>");
     res.end("<p>Hello Server</p>")
+}).listen(8088, ()=>{
+    console.log("8088번 포트에서 서버가 대기 중입니다.");
 });
 
-server.listen(8088);
-//이벤트
-server.on('listening', ()=>{
-    console.log("8088번 포트에서 서버가 대기 중입니다.")
-});
-
-server.on('error', (error)=>{
-    console.error(error);
-});
