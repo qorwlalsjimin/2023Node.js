0b94603fe8bb624b47141115f22463c4e8832258
Jimin Baek <qorwlalsjimin@naver.com>
2023-03-24 10:19:56 +0900

    서버 정보 html에 보이게 하기(안 됨)

--- week3/methodTest.js
+++ week3/methodTest.js
@@ -7,7 +7,10 @@ server.on('request', (req, res)=>{
     console.log("method: ", req.method);
     console.log("url1: ", req.url);
     console.log("url2: ", url.parse(req.url));
-    // console.log("url2-1: ", url.parse(req.url));
+    console.log("url2-1: ", url.parse(req.url).pathname);
+    console.log("url3: ", url.parse(req.url).query);
+    console.log("url4: ", url.parse(req.url, true).query);
+    let queryObj = url.parse(req.url, true).query;
 
     if(req.method == "GET"){
         console.log("GET 요청입니다.");
@@ -15,10 +18,13 @@ server.on('request', (req, res)=>{
     else if(req.method == "POST"){
         console.log("POST 요청입니다.");
     }
-
+    
+    //값이 안 나옴
     res.writeHead(200,{'content-type':'text/html'});
     res.write('<!DOCTYPE html><html lange="en" <head><meata charset="UTF-8"</head><body>');
     res.write(`<h1>A type of request is ${req.method}. </h1>`);
+    res.write(`<h1>A path is ${url.parse(req.url).pathname}. </h1>`);
+    res.write(`<h1>A query is ${JSON.stringify(queryObj)}. </h1>`);
     res.end('</body></html>');
 })
 server.listen(3000, ()=>{
