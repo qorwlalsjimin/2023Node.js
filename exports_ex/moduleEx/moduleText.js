dd2bee8d8f93d975df6a1a2fca8b6dba3dc091e6
Jimin Baek <qorwlalsjimin@naver.com>
2023-04-11 11:00:08 +0900

    텍스트, 함수 exports & require

---
+++ exports_ex/moduleEx/moduleText.js
@@ -0,0 +1,5 @@
+let text_odd = "홀수입니다.";
+let text_even = "짝수입니다.";
+
+exports.text_odd = text_odd;
+exports.text_even = text_even;
---
+++ exports_ex/moduleEx/useModule2.js
@@ -0,0 +1,6 @@
+const [check_str_OE, check_num, text_odd, text_even] = require('./useModule');
+
+console.log("*", check_str_OE("안녕"));
+console.log("*", check_num(5));
+console.log("*", text_odd);
+console.log("*", text_even);
