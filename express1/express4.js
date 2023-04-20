9d13a0e50b13bc8df5f9cd69375ec4957d64cd40
Jimin Baek <qorwlalsjimin@naver.com>
2023-04-21 08:42:19 +0900

    express의 use 여러 개 쓰기

--- express1/express4.js
+++ express1/express4.js
@@ -1,13 +1,19 @@
-//npm init
-//npm install express
-
 //모듈 가져오기
 const express = require("express");
 
 //서버 생성하기
 const app = express();
  
+app.use(function(req, res, next){
+    console.log("첫번째");
+    next(); //다음 use로 가
+}); 
+app.use(function(req, res, next){
+    console.log("두번째");
+    next();
+});
 app.use(function(req, res){
+    console.log("세번째");
     let name = req.query.name;
     let area = req.query.area; //url을 query라고 표현하기도 함
 
