802f8b0226a49edb06b1b607287f588268ae5d47
Jimin Baek <qorwlalsjimin@naver.com>
2023-04-21 08:57:49 +0900

    지정하지 않은 페이지에 접속했을때 에러화면 표시

---
+++ express1/express6.js
@@ -0,0 +1,30 @@
+//모듈 가져오기
+const express = require("express");
+
+//서버 생성하기
+const app = express();
+
+//rounter(경로) 미들웨어 사용하기
+// - express 모듈에 내장되어 있음
+app.get('/', function(req, res){
+    res.send(`<h1>페이지</h1>
+        <a href='/aaa'>aaa</a>
+        <a href='/bbb'>bbb</a>
+    `);
+});
+
+app.get('/aaa', function(req, res){
+    res.send("<h1>aaa페이지</h1>")
+});
+
+app.get('/bbb', function(req, res){
+    res.send("<h1>bbb페이지</h1>")
+});
+
+app.all('*', function(req, res){
+    res.status(404).send("<h1>ERROR-페이지를 찾을 수 없습니다.</h1>");
+})
+
+app.listen(8889, ()=>{
+    console.log("port 8889");
+})
