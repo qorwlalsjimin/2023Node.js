e9dab9c6e1a2958c844ee47b8c25a353079bf21b
Jimin Baek <qorwlalsjimin@naver.com>
2023-05-26 09:59:18 +0900

    로그인하면 파일 업로드 가능 (multer, session)

--- ExpressExample/app12-1.js
+++ ExpressExample/app12-1.js
@@ -22,6 +22,13 @@ var cookieParser = require('cookie-parser');
 // Session 미들웨어 불러오기
 var expressSession = require('express-session');
 
+// **파일 업로드용 미들웨어
+var multer = require('multer');
+var fs = require('fs');
+
+// **클라이언트에서 ajax로 요청 시 CORS(다중 서버 접속)지원
+var cors = require('cors');
+
 //express 객체 생성
 var app = express();
 app.set('port', process.env.PORT || 3000);
@@ -29,7 +36,14 @@ app.set('port', process.env.PORT || 3000);
 //body-parser
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+//** 추가2
+app.use(static(path.join(__dirname, 'uploads')));
 app.use("/", static(path.join(__dirname, "public"))); // "/" 안 써도 똑같음
+
+//** 추가3
+//클라이언트에서 ajax로 요청 시 CORS(다중서버접속)지원
+app.use(cors());
 
 // 세션 설정
 app.use(expressSession({
@@ -48,6 +62,7 @@ router.route('/').get(function(req, res){
     res.end();
 });
 
+// 로그인
 router.route('/process/login').post(function(req, res){
     console.log('/process/login 호출됨');
     
@@ -72,14 +87,18 @@ router.route('/process/login').post(function(req, res){
         res.write("<div><p>Param id : " + paramId + "</p></div>");
         res.write("<div><p>Param password : " + paramPassword + "</p></div>");
         res.write(
-          "<br><br><button><a href='/process/product'>상품 페이지로 이동하기</a></button>"
+          "<br><br><button><a href='/process/product'>상품 페이지로 이동하기</a></button"
         );
+        res.write(
+            "<br><br><button><a href='/photomulti3300.html'>파일업로드로 이동하기</a></button>"
+          );
         // router.route('/process/product').get으로 연결됨
         res.end();
     };
     //redirect로 응답
 });
 
+//로그아웃
 router.route('/process/logout').get(function(req,res){
     console.log('/process/logout 호출됨.');
 
@@ -100,6 +119,7 @@ router.route('/process/logout').get(function(req,res){
     }
 });
 
+//상품
 router.route('/process/product').get(function(req,res){
     console.log('/process/product 호출됨.');
     if(req.session.user){
@@ -108,6 +128,78 @@ router.route('/process/product').get(function(req,res){
         res.redirect('/login2.html');
     }
 });
+
+//이동1
+//multer 미들웨어 사용: 미들웨어 사용 순서 중요 body-parser -> multer -> router
+//파일 제한: 10개, 1G
+var storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, 'uploads')
+    },
+    filename: function(req, file, callback){
+        /* callback(null, file.originalname + Date.now()) */
+        //callback(null, file.originalname)
+        var extension = path.extname(file.originalname);
+        var basename = path.basename(file.originalname, extension);
+        callback(null, basename+Date.now()+extension);
+    }
+});
+
+var upload = multer({
+    storage: storage,
+    limits: {
+        files: 12,
+        files: 1024 * 1024 * 1024
+    }
+});
+
+//추가5
+//파일 업로드 라우팅 함수 - 로그인 후 세션 저장함
+router.route('/process/photo12').post(upload.array('photo12', 12), function(req, res){
+    console.log('/process/photomulti3300 호출됨');
+
+    res.writeHead('200', {'Content-Type':'text/html; charset=utf8'});
+    try{
+        var files = req.files;
+
+        //현재의 파일 정보를 저장할 변수 선언
+        var originalname = '', 
+        filename = '', 
+        mimetype='', 
+        size=0;
+
+        if(Array.isArray(files)){ //배열에 들어간 경우 - 설정에서 1개의 파일도 배열에 넣게 함
+            console.log('배열에 들어있는 파일 갯수: %d', files.length);
+
+            for(var index = 0; index < files.length; index++){
+                console.dir(`#==== 업로드된 ${index+1}번째 파일 정보 ====#`)
+                originalname = files[index].originalname;
+                filename = files[index].filename;
+                mimetype = files[index].mimetype;
+                size = files[index].size;
+                console.log(`현재 파일 정보: ${originalname}, ${filename}, ${mimetype}, ${size}`)
+                    
+                //클라이언트에 응답 전송
+                res.write('<h3>파일 업로드 성공</h3>');
+                res.write('<hr>');
+                res.write(`<p>원본파일명: ${originalname} -> 저장 파일명: ${filename}</p>`);
+                res.write(`<p>MIME TYPE: ${mimetype}</p>`);
+                res.write(`<p>파일 크기: ${size}</p>`);
+                res.end();
+            }//for-end
+            res.write("<br><br><button type=button><a href='/process/product'>상품 페이지로 이동하기</a></button>");
+        }else{ //배열에 들어가 있지 않은 경우 (현재 설정에서는 해당 없음)
+            console.log("파일 갯수: 1");
+            
+            originalname = files[index].originalname;
+            filename = files[index].filename;
+            mimetype = files[index].mimetype;
+            size = files[index].size;
+        } //if-end
+    }catch(err){
+        console.dir(err.stack);
+    }//try-catch-end
+})
 
 //라우터 객체를 app 객체에 등록
 app.use('/', router);
